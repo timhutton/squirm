63cb8e6c45f589b7372518ba3c17ad4e526d08af
Tim Hutton <tim.hutton@gmail.com>
2023-02-11 18:24:56 +0000

    Made get_move_candidates()

--- 3d/src/main.js
+++ 3d/src/main.js
@@ -30,17 +30,17 @@ window.onload = function() {
     const cube_material = new THREE.MeshStandardMaterial( { color: 0xffffff, wireframe: false } );
     let cube = new THREE.BoxGeometry(1,1,1);
 
-    let X = 10;
-    let Y = 10;
-    let Z = 10;
+    let X = 20;
+    let Y = 20;
+    let Z = 20;
     let N = 40;
     let mesh = new THREE.InstancedMesh( cube, cube_material, N );
     mesh.instanceMatrix.setUsage( THREE.DynamicDrawUsage ); // will be updated every frame
     scene.add( mesh );
 
-    camera.position.x = 35;
-    camera.position.y = 46;
-    camera.position.z = 65;
+    camera.position.x = 15;
+    camera.position.y = 26;
+    camera.position.z = 25;
 
     // initialize empty grid
     let grid = [];
@@ -54,6 +54,8 @@ window.onload = function() {
         }
     }
 
+    function p(x, y, z) { return {x: x, y: y, z: z}; }
+
     // add cubes at random unoccupied locations
     let pos = [];
     for(let i = 0; i < N; i++) {
@@ -63,7 +65,7 @@ window.onload = function() {
             z = Math.floor(Math.random() * Z);
         } while(grid[x][y][z] != 0);
         grid[x][y][z] = 1;
-        pos[i] = {x: x, y: y, z: z};
+        pos[i] = p(x, y, z);
     }
 
     orbit_controls = new THREE.OrbitControls( camera, renderer.domElement );
@@ -85,22 +87,37 @@ window.onload = function() {
         renderer.render( scene, camera );
     }
 
+    function get_move_candidates(i) {
+        // return a list of the unoccupied cells within one step
+        const x0 = Math.max(0, pos[i].x - 1);
+        const x1 = Math.min(X-1, pos[i].x + 1);
+        const y0 = Math.max(0, pos[i].y - 1);
+        const y1 = Math.min(Y-1, pos[i].y + 1);
+        const z0 = Math.max(0, pos[i].z - 1);
+        const z1 = Math.min(Y-1, pos[i].z + 1);
+        let candidates = [];
+        for(let x = x0; x <= x1; x++) {
+            for(let y = y0; y <= y1; y++) {
+                for(let z = z0; z <= z1; z++) {
+                    if(grid[x][y][z] == 0)
+                        candidates.push(p(x, y, z));
+                }
+            }
+        }
+        return candidates;
+    }
+
     function move_cube(i) {
-        // pick a random step
-        let x = pos[i].x + Math.floor(Math.random() * 3) - 1;
-        let y = pos[i].y + Math.floor(Math.random() * 3) - 1;
-        let z = pos[i].z + Math.floor(Math.random() * 3) - 1;
-        // reject if off-grid or occupied
-        if( x < 0 || x >= X || y < 0 || y >= Y || z < 0 || z >= Z )
-            return false;
-        if(grid[x][y][z] > 0)
-            return false;
+        // pick a random move
+        const move_candidates = get_move_candidates(i);
+        const iMove = Math.floor(Math.random() * move_candidates.length);
+        const move = move_candidates[iMove];
         // move
+        grid[move.x][move.y][move.z] = grid[pos[i].x][pos[i].y][pos[i].z];
         grid[pos[i].x][pos[i].y][pos[i].z] = 0;
-        grid[x][y][z] = 1;
-        pos[i].x = x;
-        pos[i].y = y;
-        pos[i].z = z;
+        pos[i].x = move.x;
+        pos[i].y = move.y;
+        pos[i].z = move.z;
         return true;
     }
 
